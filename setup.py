632dcf990ed2cc9e55725d6a08c5cca32253ded1
Sam Claassens <sam@globus.org>
2021-12-01 15:37:33 -0600

    Updating version to match the tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sqlalchemy_softdelete",
-    version="0.2",
+    version="1.1",
     url="http://github.com/mattias-lidman/sqlalchemy_softdelete",
     license="BSD",
     description="Soft deletes in SQLAlchemy",
